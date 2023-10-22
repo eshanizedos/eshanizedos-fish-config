2f7df8901579f77a4ec326bb08a0aa509d7c655b
ESHANIZED <148610067+eshanized@users.noreply.github.com>
2023-10-22 08:58:49 +0530

    Create fish_prompt.fish

---
+++ etc/skel/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,29 @@
+function fish_prompt
+    set -l var (tty)
+    switch $var
+        case '*/dev/tty*'
+            set_color 00ff00
+            echo -n "[EZ:"
+            set_color ff00d7
+            echo -n "$(ip -4 addr | grep -v '127.0.0.1' | grep -v 'secondary' | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -z 's/\n/|/g;s/|\$/\n/' | rev | cut -c 2- | rev)"
+            echo -n " | $USER"
+            set_color 00ff00
+            echo "]"
+            set_color 00ff00
+            echo -n "[>]"
+            set_color 00ffff
+            echo (pwd) '$' (set_color normal)
+        case '*'
+            set_color 00ff00
+            echo -n "┌──[EZ🚬"
+            set_color ff00d7
+            echo -n "$(ip -4 addr | grep -v '127.0.0.1' | grep -v 'secondary' | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -z 's/\n/|/g;s/|\$/\n/' | rev | cut -c 2- | rev)"
+            echo -n "🔥$USER"
+            set_color 00ff00
+            echo "]"
+            set_color 00ff00
+            echo -n "└──╼[🦁]"
+            set_color 00ffff
+            echo (pwd) '$' (set_color normal)
+    end
+    end
